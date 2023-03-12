1eea40db60e471caee395d401bb6da15f503ca5d
Nadezhda.S <nadviks@mail.ru>
2023-03-12 21:53:26 +0300

    Max из двух чисел

---
+++ DZ_Z2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает 
+// два числа и выдает, какое число большее, а какое меньшее.
+Console.Clear();
+Console.Write("Введите число A: ");
+int A = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число B: ");
+int B = int.Parse(Console.ReadLine()!);
+if (A > B)
+{
+    int max = A;
+    Console.WriteLine($"max = {A}");
+}
+else if (B > A)
+{
+    int max = B;
+    Console.WriteLine($"max = {B}");
+}
+else 
+{
+    Console.WriteLine($"Числа равны {A} = {B}");
+}
