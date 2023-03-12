2e2361d390d30037edaddffbf6e3a303406d1cd3
Nadezhda.S <nadviks@mail.ru>
2023-03-12 22:05:41 +0300

    Задача 4. Max из трех чисел

---
+++ DZ_Z4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход 
+// три числа и выдает максимальное из этих чисел.
+Console.Clear();
+Console.Write("Введите число A: ");
+int A = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число B: ");
+int B = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число C: ");
+int C = int.Parse(Console.ReadLine()!);
+int max = A;
+if (B > A)
+{
+    max = B;
+}
+else if (C > B)
+{
+    max = C;
+}
+Console.WriteLine($"Максимальное из чисел : {max}");
