2d3de9b08a898964b34cc203dace34d8a667d76a
Nadezhda.S <nadviks@mail.ru>
2023-03-12 16:27:19 +0300

    Z2 - Проверяет, первое квадрат второго

---
+++ Z2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает два числа и проверяет,
+// является ли первое число квадратом второго.
+// a = 25, b = 5 -> да  // a = 2, b = 10 -> нет 
+
+Console.Clear();
+Console.Write("Введите число a: ");
+int numberA = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число b: ");
+int numberB = int.Parse(Console.ReadLine()!);
+
+if (numberA == numberB * numberB)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
